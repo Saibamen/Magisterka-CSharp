45fc30065d7a79c843c9410bcc13cd743646f19d
Saibamen <saibamenppl@gmail.com>
2020-07-12 00:48:13 +0200

    Move to FileModule

---
+++ CSharp/FileModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace CSharp
+{
+    public class FileModule
+    {
+        private static readonly string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        private static readonly string ReadTestFile = $"{BaseDirectory}{Path.DirectorySeparatorChar}da51f72f-7804-40fe-bc66-8fc5418325fb_001.data";
+
+        public static void ReadFile_ReadAllText()
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            for (var i = 0; i < Program.Iterations; i++)
+            {
+                File.ReadAllText(ReadTestFile);
+            }
+
+            stopwatch.Stop();
+            Program.PrintElapsedTime(stopwatch);
+        }
+
+        public static void ReadFile_ByLine()
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            for (var i = 0; i < Program.Iterations; i++)
+            {
+                var fileStream = new FileStream(ReadTestFile, FileMode.Open);
+                using var streamReader = new StreamReader(fileStream);
+                streamReader.ReadLine();
+            }
+
+            stopwatch.Stop();
+            Program.PrintElapsedTime(stopwatch);
+        }
+    }
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,29 +1,25 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
 
 namespace CSharp
 {
     public class Program
     {
+        public const int Iterations = 1000;
         private const int TestAttempts = 10;
-        private const int Iterations = 1000;
-
-        private static readonly string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-        private static readonly string ReadTestFile = $"{BaseDirectory}{Path.DirectorySeparatorChar}da51f72f-7804-40fe-bc66-8fc5418325fb_001.data";
 
         private static void Main()
         {
             for (var i = 0; i < TestAttempts; i++)
             {
-                ReadFile_ReadAllText();
+                FileModule.ReadFile_ReadAllText();
             }
 
             Console.WriteLine();
 
             for (var i = 0; i < TestAttempts; i++)
             {
-                ReadFile_ByLine();
+                FileModule.ReadFile_ByLine();
             }
 
             Console.WriteLine();
@@ -31,35 +27,7 @@ namespace CSharp
             Console.ReadKey();
         }
 
-        private static void ReadFile_ReadAllText()
-        {
-            var stopwatch = Stopwatch.StartNew();
-
-            for (var i = 0; i < Iterations; i++)
-            {
-                File.ReadAllText(ReadTestFile);
-            }
-
-            stopwatch.Stop();
-            PrintElapsedTime(stopwatch);
-        }
-
-        private static void ReadFile_ByLine()
-        {
-            var stopwatch = Stopwatch.StartNew();
-
-            for (var i = 0; i < Iterations; i++)
-            {
-                var fileStream = new FileStream(ReadTestFile, FileMode.Open);
-                using var streamReader = new StreamReader(fileStream);
-                streamReader.ReadLine();
-            }
-
-            stopwatch.Stop();
-            PrintElapsedTime(stopwatch);
-        }
-
-        private static void PrintElapsedTime(Stopwatch stopwatch)
+        public static void PrintElapsedTime(Stopwatch stopwatch)
         {
             var stackTrace = new StackTrace();
             var callingMethod = stackTrace.GetFrame(1)?.GetMethod()?.Name;
